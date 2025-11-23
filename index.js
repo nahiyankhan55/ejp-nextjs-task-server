5e4123d1ce8b6c99db66f76fcc59b7ef5ebfd75f
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2025-11-24 03:06:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3030;
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: ["*"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.bfqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Connections
+    const database = client.db(process.env.DB_NAME);
+    const usersCollection = database.collection("users");
+    const productsCollection = database.collection("products");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("E.martBD server");
+});
+
+app.listen(port, () => {
+  console.log(`E.martBD server listening on port ${port}`);
+});

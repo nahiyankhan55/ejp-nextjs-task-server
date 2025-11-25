d61780d763860aae57f407d54f985d41eda33d63
nahiyankhan55 <kushbuladesignmax+++@gmail.com>
2025-11-25 19:29:31 +0600

    deploy push

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const app = express();
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://ejp-nextjs-emartbd-client.vercel.app",
+    ],
     credentials: true,
   })
 );
@@ -32,12 +35,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     // Connections
     const database = client.db(process.env.DB_NAME);
